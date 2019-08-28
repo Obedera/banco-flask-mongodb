ae2fb5a2ef1fdf483b9f923c2d36ecba1aa0e44a
Obede <obede.silva7@gmail.com>
2019-08-27 21:35:36 -0300

    pagina user

--- run.py
+++ run.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import datetime
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/",methods = ['POST','GET'])
 def index():
+    if request.method == 'POST':
+        result = request.form
+        if result['email'] == 'obede' and result['senha'] == '1234':
+            return render_template('user.html',email=result['email'],senha=result['senha'])
+        else:
+            return render_template('index.html',msg='Login invalido')
+
     return render_template('index.html')
 
 if __name__ == '__main__':
