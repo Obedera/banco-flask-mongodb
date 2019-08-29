eb615e0777acd8cdc88fe040fc542f3f3cd500b2
Obede <obede.silva7@gmail.com>
2019-08-29 14:27:49 -0300

    tabelas bd

--- run.py
+++ run.py
@@ -15,7 +15,8 @@ def index():
         user = request.form
         user_bd = mongo.db.pessoa.find_one({'email':user.get('email'),'senha':user.get('senha')})
         if user_bd is not None:
-            return render_template('user.html',user=user_bd)
+            conta = mongo.db.conta.find_one({'id_user':user_bd['_id']})
+            return render_template('user.html',user=user_bd,conta=conta)
         else:
             return render_template('index.html',msg='Login invalido')
 
@@ -28,7 +29,14 @@ def cadastrar():
         user = request.form
         user_bd = mongo.db.pessoa.find_one({'email':user.get('email')})
         if user_bd is None:
-            mongo.db.pessoa.insert({'nome' : user.get('nome'), 'sobrenome' : user.get('sobrenome'), 'email' : user.get('email'), 'senha' : user.get('senha'), 'saldo': 0})
+            data = f'{datetime.datetime.now().day}/{datetime.datetime.now().month}/{datetime.datetime.now().year}'
+            hora = f'{datetime.datetime.now().hour}:{datetime.datetime.now().minute}'
+
+            mongo.db.pessoa.insert({'nome' : user.get('nome'), 'sobrenome' : user.get('sobrenome'), 'email' : user.get('email'), 'senha' : user.get('senha')})
+
+            id_user = mongo.db.pessoa.find_one({'email':user.get('email')})
+
+            mongo.db.conta.insert({'id_user': id_user['_id'], 'data':data, 'hora':hora, 'ContaInicial':0, 'ContaFinal':0, 'Valor':0})
             return render_template('index.html',msg='Faça seu login agora')
         else:
             return render_template('cadastro.html',msg='Usuário já cadastrado')
